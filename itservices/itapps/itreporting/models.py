f0440eb8a57340254c586e7edc15d204d1fa915d
rascove <rascove@yahoo.com>
2023-03-01 17:45:07 +0000

    Completed the application

--- itservices/itapps/itreporting/models.py
+++ itservices/itapps/itreporting/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
@@ -12,3 +13,6 @@ class Issue(models.Model):
 
     def __str__(self):
         return self.type
+
+    def get_absolute_url(self):
+        return reverse('issue-detail', kwargs={'pk': self.pk})
